88160727f10eb4231e0d5c081b48a78b0a4228d5
heba thaher <67863629+hebathaher@users.noreply.github.com>
2021-07-04 03:17:10 -0400

    Update create.sql

--- create.sql
+++ create.sql
@@ -1,6 +1,6 @@
 CREATE TABLE university.student_courses
  (
-	id INT AUTO_INCREMENT,
+	id INT UNSIGNED AUTO_INCREMENT,
        student_id INT NOT NULL,
        course_id  INT NOT NULL,
 	year SMALLINT NOT NULL,
@@ -11,7 +11,7 @@ CREATE TABLE university.student_courses
 
 CREATE TABLE university.countries
  (
-      id INT AUTO_INCREMENT
+      id INT UNSIGNED AUTO_INCREMENT
 	name VARCHAR(30) NOT NULL,
       CONSTRAINT countries_PK PRIMARY KEY (id)
 );
@@ -21,18 +21,18 @@ CREATE TABLE university.countries
 
 
 CREATE TABLE university.courses (
-	id INT AUTO_INCREMENT,
+	id INT UNSIGNED AUTO_INCREMENT,
 	course_number VARCHAR(5) NOT NULL,
 	title VARCHAR(30) NOT NULL,
 	description TEXT NOT NULL,
 	credit_hours TINYINT NOT NULL,
-	teacher_id INT NOT NULL,
+	teacher_id INT UNSIGNED NOT NULL,
 	CONSTRAINT courses_PK PRIMARY KEY (id)
 );
 
 CREATE TABLE university.teachers
  (
-	id INT AUTO_INCREMENT,
+	id INT UNSIGNED  AUTO_INCREMENT,
 	employment_number VARCHAR(10) NOT NULL,
 	name VARCHAR(30) NOT NULL,
 	CONSTRAINT teachers_PK PRIMARY KEY (id)
@@ -41,9 +41,9 @@ CREATE TABLE university.teachers
 CREATE TABLE university.students
 
  (
-	id INT AUTO_INCREMENT,
+	id INT UNSIGNED AUTO_INCREMENT,
 	registration_number VARCHAR(10) NOT NULL,
 	name VARCHAR(30) NOT NULL,
-      country_id INT NOT NULL,
+      country_id UNSIGNED INT NOT NULL,
 	CONSTRAINT students_PK PRIMARY KEY (id)
 );
